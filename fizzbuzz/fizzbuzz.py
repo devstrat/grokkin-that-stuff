ddf46da15362ccb7d27f9e37b2e01dabb75513bc
devstrat <dmitry.evstratov@gmail.com>
2017-12-10 22:58:13 +0100

    added fizzbuzz

---
+++ fizzbuzz/fizzbuzz.py
@@ -0,0 +1,7 @@
+def fizzbuzz(num: int):
+    return 'Fizz' * (num % 3 == 0) + 'Buzz' * (num % 5 == 0) or num
+
+
+if __name__ == '__main__':
+    for item in range(101):
+        print(fizzbuzz(item))
